544545a6f38e7d3088ea443525fcdf149ccaa7c5
Luis Chang <luis.chang@outlook.com>
2021-05-21 22:37:42 -0500

    Agregar archivos de proyecto.

---
+++ M2_NETCoreMVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using M2_NETCoreMVC.Web.Areas.Marketing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M2_NETCoreMVC.Web.Areas.Marketing.Controllers
+{
+    [Area("Marketing")]
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductsWithViewModel()
+        {
+            var products = GetProductsJsonLocal();
+            return View(products);
+        }
+
+        public IActionResult ProductsWithViewBag()
+        {
+            var products = GetProductsJsonLocal();
+            ViewBag.ProductList = products;
+
+            return View();
+        }
+
+        public IActionResult ProductsWithViewData()
+        {
+            var products = GetProductsJsonLocal();
+            ViewData["ProductList"] = products;
+
+            return View();
+        }
+
+
+
+        public List<Product> GetProductsJsonLocal()
+        {
+            var folderDetails = Path.Combine(Directory.GetCurrentDirectory(), $"Areas\\Marketing\\Data\\Products.json");
+            var json = System.IO.File.ReadAllText(folderDetails);
+
+            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+            return products;
+        }
+
+
+
+    }
+}
